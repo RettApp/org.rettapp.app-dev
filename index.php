8e87f2a798d7003fa361934929d87e2527907b14
Jonathan Starck <support@cretection.eu>
2016-04-21 08:20:44 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<!-- !Copyright -->
+<!--
+	Project: org.rettapp.app-dev
+	Description: Der Universalhelper für Rettungsdienstler
+	Author: Jonathan Starck 
+	Licence: GNU GENERAL PUBLIC LICENSE Version 2, June 1991 & European Union Public Licence
+	Source: https://github.com/RettApp/org.rettapp.app-dev
+-->
+</html>
